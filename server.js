078904ac7f1b32653f0f613dfd001a966c43a003
ahmed yasser <ahmedya3ser8@gmail.com>
2026-01-25 13:29:32 +0200

    feat: fix webhooks-checkout

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use((req, res, next) => {
 
 app.use(globalError);
 
-const server = app.listen(process.env.PORT || 4000, () => {
+const server = app.listen(process.env.PORT, () => {
   console.log(`App Running on port ${process.env.PORT}`);
 })
 
